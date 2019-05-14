617475173377482f5cff0c3749098345cee5fb10
LAS <dkavialkou@elinext.com>
2019-05-14 11:55:20 +0300

    something in Authorization

--- src/main/java/com/http/webservice/controller/client/Authorization.java
+++ src/main/java/com/http/webservice/controller/client/Authorization.java
@@ -3,6 +3,7 @@ package com.http.webservice.controller.client;
 import com.http.webservice.controller.tools.Forward;
 import com.http.webservice.entity.User;
 import com.http.webservice.service.impl.ClientServiceImpl;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,9 +13,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import javax.servlet.http.HttpSession;
 
 @Controller
+@AllArgsConstructor
 public class Authorization {
-    @Autowired
-    private ClientServiceImpl service;
+    private final ClientServiceImpl service;
 
     @PostMapping("/login")
     public String logIn(@RequestParam String login, @RequestParam String password, Model model, HttpSession session) {
